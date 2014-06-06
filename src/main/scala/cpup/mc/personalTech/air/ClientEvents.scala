c3291a35b0af5920729142f76b83c376a25b3e3a
CoderPuppy <cpup@cpup-laptop>
2014-06-06 15:49:53 -0400

    Fix position and comment out debug stuff

--- src/main/scala/cpup/mc/personalTech/air/ClientEvents.scala
+++ src/main/scala/cpup/mc/personalTech/air/ClientEvents.scala
@@ -17,18 +17,20 @@ class ClientEvents {
 
 	@SubscribeEvent
 	def renderOverlay(e: RenderGameOverlayEvent) {
+//		mod.logger.debug("is = {}, get = {}", choosePowerKeyBind.isPressed, choosePowerKeyBind.getIsKeyPressed)
 		if(choosePowerKeyBind.getIsKeyPressed) {
 			val tess = Tessellator.instance
 			val mc = Minecraft.getMinecraft
-			val screenWidth = Display.getWidth
-			val screenHeight = Display.getHeight
+			val screenWidth = mc.displayWidth
+			val screenHeight = mc.displayHeight
 
 			tess.setColorOpaque(255, 255, 255)
 			tess.startDrawingQuads()
 			val width = 50
 			val height = 50
-			val x = (screenWidth / 2) + (width / 2)
-			val y = (screenHeight / 2) + (height / 2)
+//			println("s.w, s.h", screenWidth, screenHeight, width, height)
+			val x = (screenWidth - width) / 2
+			val y = (screenHeight - height) / 2
 			tess.addVertex(x,         y,          0)
 			tess.addVertex(x,         y + height, 0)
 			tess.addVertex(x + width, y + height, 0)
