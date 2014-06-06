43fbae31fe615193ac052c1e9456e01c93090289
CoderPuppy <cpup@cpup-laptop>
2014-06-06 15:12:57 -0400

    This might help it load

--- src/main/scala/cpup/mc/personalTech/PersonalTech.scala
+++ src/main/scala/cpup/mc/personalTech/PersonalTech.scala
@@ -1,7 +1,9 @@
 package cpup.mc.personalTech
 
 import cpup.mc.lib.CPupMod
+import cpw.mods.fml.common.Mod
 
+@Mod(modid = Ref.modID, modLanguage = "scala")
 object PersonalTech extends CPupMod[Ref.type] {
 	override def ref = Ref
 }
