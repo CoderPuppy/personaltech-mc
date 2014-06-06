9ffdc35f6ea355e463f1e838d736595b6170589d
CoderPuppy <cpup@cpup-laptop>
2014-06-06 17:52:45 -0400

    Draw the modes

--- src/main/scala/cpup/mc/personalTech/air/ClientEvents.scala
+++ src/main/scala/cpup/mc/personalTech/air/ClientEvents.scala
@@ -7,13 +7,14 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent
 import net.minecraftforge.client.event.RenderGameOverlayEvent
 import net.minecraft.client.renderer.Tessellator
 import net.minecraft.client.Minecraft
-import org.lwjgl.opengl.Display
 import cpw.mods.fml.client.registry.ClientRegistry
+import cpw.mods.fml.common.gameevent.TickEvent
 
 class ClientEvents {
 	def mod = PersonalTech
 	val choosePowerKeyBind = new KeyBinding(s"key.${mod.ref.modID}:air.choosePower", Keyboard.KEY_V, s"key.categories.${mod.ref.modID}")
 	ClientRegistry.registerKeyBinding(choosePowerKeyBind)
+	var wasChoosePowerDown = false
 
 	@SubscribeEvent
 	def renderOverlay(e: RenderGameOverlayEvent) {
@@ -24,9 +25,9 @@ class ClientEvents {
 			val screenWidth = mc.displayWidth / 2
 			val screenHeight = mc.displayHeight / 2
 
-			tess.startDrawingQuads()
+			tess.startDrawingQuads
 			tess.setColorOpaque(0, 0, 0)
-			val width = 50
+			val width = 200
 			val height = 50
 //			println("s.w, s.h", screenWidth, screenHeight, width, height)
 			val x = (screenWidth - width) / 2
@@ -35,7 +36,21 @@ class ClientEvents {
 			tess.addVertex(x,         y + height, 0)
 			tess.addVertex(x + width, y + height, 0)
 			tess.addVertex(x + width, y,          0)
-			tess.draw()
+			tess.draw
+			var modeX = x + 2
+			mc.fontRenderer.drawStringWithShadow("Disabled", modeX, y + 2, 16777215)
+			modeX += mc.fontRenderer.getStringWidth("Disabled") + 2
+			mc.fontRenderer.drawStringWithShadow("Attack", modeX, y + 2, 16777215)
+			modeX += mc.fontRenderer.getStringWidth("Attack")
+			mc.fontRenderer.drawStringWithShadow("Harvest", modeX, y + 2, 16777215)
+			modeX += mc.fontRenderer.getStringWidth("Harvest")
+		}
+	}
+
+	@SubscribeEvent
+	def renderTick(e: TickEvent.RenderTickEvent) {
+		if(e.phase == TickEvent.Phase.END) {
+
 		}
 	}
 }
