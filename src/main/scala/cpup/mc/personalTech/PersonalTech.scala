7d182c9b99e51a87e6538d6f7dbb6140b7255d77
CoderPuppy <cpup@cpup-laptop>
2014-06-06 15:22:42 -0400

    Use the proxies

--- src/main/scala/cpup/mc/personalTech/PersonalTech.scala
+++ src/main/scala/cpup/mc/personalTech/PersonalTech.scala
@@ -1,9 +1,20 @@
 package cpup.mc.personalTech
 
 import cpup.mc.lib.CPupMod
-import cpw.mods.fml.common.Mod
+import cpw.mods.fml.common.{SidedProxy, Mod}
+import cpw.mods.fml.common.event.FMLPreInitializationEvent
+import cpw.mods.fml.common.Mod.EventHandler
 
 @Mod(modid = Ref.modID, modLanguage = "scala")
 object PersonalTech extends CPupMod[Ref.type] {
 	override def ref = Ref
+
+	@SidedProxy(clientSide = "cpup.mc.personalTech.client.ClientProxy", serverSide = "cpup.mc.personalTech.CommonProxy")
+	var proxy: CommonProxy = null
+
+	@EventHandler
+	override def preInit(e: FMLPreInitializationEvent) {
+		super.preInit(e)
+		proxy.registerEvents
+	}
 }
